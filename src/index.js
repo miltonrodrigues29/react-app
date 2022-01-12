10f66e5c36e8a9c867f9ab0a34a8d3f60d49356d
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-01-12 18:53:48 +0530

    -

--- src/index.js
+++ src/index.js
@@ -18,31 +18,22 @@ function Booklist() {
   );
 }
 
+const author = "James Clear";
 const Book = () => {
+  const title = "Atomic Habits";
   return (
     <article className="book">
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+      <img
+        src="https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
+        alt=""
+      ></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <p>{6 + 6}</p>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt=""
-  ></img>
-);
-
-const Title = () => <h1>Atomic Habits</h1>;
-const Author = () => <h4>James Clear</h4>;
-
-const Person = () => (
-  <h2 style={{ color: "#617d98", fontSiz: "0.75rem", marginTo: "0.25rem" }}>
-    Milton Rodrigues
-  </h2>
-);
 const Message = () => {
   return <p>this is my message</p>;
 };
