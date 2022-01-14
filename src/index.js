e0b4f2e60a9c1849d23adf4cf99c90045386bf1f
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-01-14 19:30:13 +0530

    project complete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDom from "react-dom";
 import "./index.css";
 import { books } from "./books";
 import Book from "./Book";
+import { greeting } from "./testing/testing";
 
 // JSX rules
 //return single element //should have only one parent element
