debc226384514d8c850b0f20d92669e00e0a3c38
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-01-14 16:55:49 +0530

    -

--- src/index.js
+++ src/index.js
@@ -10,44 +10,58 @@ import "./index.css";
 //close every element
 //formatting
 //setup vars
-const author = "James Clear";
-const title = "Atomic Habits";
-const img =
-  "https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg";
+// const names = ["milton", "rodrigues"];
+// const newNames = names.map((name) => {
+//   return <h1>{name}</h1>;
+// });
+const books = [
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51-nXsSRfZL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "The Psychology of Money",
+    author: "Morgan Housel",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51T8OXMiB5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Ikigai: The Japanese secret to a long and happy life ",
+    author: "Hector Garcia",
+  },
+  {
+    id: 4,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/51u8ZRDCVoL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title:
+      "Rich Dad Poor Dad: What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not! ",
+    author: "Robert T. Kiyosaki",
+  },
+];
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book job="developer" sal={"55k"} />
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  console.log(props);
+  const { img, author, title, id } = props.book;
+
   return (
     <article className="book">
-      <img src={img} alt=""></img>
+      <h1>{id}</h1> <img src={img} alt=""></img>
       <h1>{title}</h1>
       <h4>{author}</h4>
-      <p>{6 + 6}</p>
-      <p>{props.job}</p>
     </article>
   );
 };
-
-const Message = () => {
-  return <p>this is my message</p>;
-};
-//above is easy and readble than the below chain functions
-
-// const Greeting = () => {
-//   return React.createElement(
-//     "div",
-//     {},
-//     React.createElement("h1", {}, "hello world")
-//   );
-//   return React.createElement("h1", {}, "Hello World");
-// };
 
 ReactDom.render(<Booklist />, document.getElementById("root"));
